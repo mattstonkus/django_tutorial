88c115b8fa440109bc5638599bea12fa3847584b
mattstonkus <matt.stonkus@gmail.com>
2021-08-31 11:38:42 -0700

    forgot to add this

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': "Matt",
-        'title': "First Post",
-        'content': 'First post content',
-        'date_posted': 'August 23, 2021'
-    },
-{
-        'author': "Matt",
-        'title': "Second Post",
-        'content': 'Second post content',
-        'date_posted': 'August 23, 2021'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
